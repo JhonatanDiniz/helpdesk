ff90c1bf91c2bd59d12b5fcc670a3ec7b028d34d
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2024-01-24 22:32:10 -0300

    Created method findById Tecnico

--- src/main/java/com/jhonatan/helpdesk/resources/TecnicoResources.java
+++ src/main/java/com/jhonatan/helpdesk/resources/TecnicoResources.java
@@ -1,2 +1,24 @@
-package com.jhonatan.helpdesk.resources;public class TecnicoResources {
+package com.jhonatan.helpdesk.resources;
+
+import com.jhonatan.helpdesk.domain.Tecnico;
+import com.jhonatan.helpdesk.services.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/tecnico")
+public class TecnicoResources {
+
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Tecnico> findById(@PathVariable Long id){
+        Tecnico obj = tecnicoService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
 }
--- src/main/java/com/jhonatan/helpdesk/services/TecnicoService.java
+++ src/main/java/com/jhonatan/helpdesk/services/TecnicoService.java
@@ -1,2 +1,20 @@
-package com.jhonatan.helpdesk.services;public class TecnicoService {
+package com.jhonatan.helpdesk.services;
+
+import com.jhonatan.helpdesk.domain.Tecnico;
+import com.jhonatan.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    public Tecnico findById(Long id){
+        Optional<Tecnico> obj = tecnicoRepository.findById(id);
+        return obj.orElse(null);
+    }
 }
